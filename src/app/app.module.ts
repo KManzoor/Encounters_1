75b3ccdd24a7c8f79466328dc77c276051f96cbf
Manzoor Ahmed Agharia <hussaink.14326@gmail.com>
2025-03-26 22:37:45 +0530

    ADDED HTTPCLient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-// import { NgModule } from "@angular/core";
-// import { BrowserModule } from "@angular/platform-browser";
-// import { CommonModule } from "@angular/common";
-// import { AppComponent } from "./app.component";
-// import { MainDashboardComponent } from "./components/main-dashboard/main-dashboard.component";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from '@angular/common/http';
 
-// @NgModule({
-//     declarations: [AppComponent],
-//     imports: [BrowserModule, CommonModule],
-//     bootstrap: [AppComponent]
-// })
-// export class AppModule {}
+@NgModule({
+    declarations: [AppComponent],
+    imports: [BrowserModule, CommonModule,HttpClientModule],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(AppComponent,  {providers: [provideRouter(routes)]})
+bootstrapApplication(AppComponent,  {providers: [provideRouter(routes),provideHttpClient()]})
   .catch((err) => console.error(err));
